930c7987609ae4faeb78c1cb8b74d299a120cb9e
bassistjune <75768774+bassistjune@users.noreply.github.com>
2021-09-30 15:19:20 +0900

    Add files via upload
    test

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     });
     // 뉴스슬라이드
     $('.news_slide').slick({
-        arrows: false, 
+        arrows: true, 
         speed: 300,
         responsive: [
             {
@@ -15,6 +15,8 @@
                 slidesToScroll: 1,
                 infinite: true,
                 arrows: false,
+                prevArrow : ".news-slick-prev",		
+                nextArrow : ".news-slick-next",
               }
             }]
     });
@@ -57,57 +59,67 @@
           // instead of a settings object
         ]
       });
-       // 배너슬라이드
-    $('.banner-slide').slick({
+      bannerSlide();
+      function bannerSlide() {
+
+        // 배너슬라이드
+        $('.banner-slide').slick({
+          
+          dots: false,
+          infinite: true,
+          arrows: true,
+          autoplay : true,
+          autoplaySpeed: 1000,
+          easing: 'linear',
+          cssEase: 'linear',
+          speed: 1000,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          prevArrow : ".banner-slick-prev",		
+          nextArrow : ".banner-slick-next",
+          responsive: [
+            {
+              breakpoint: 1006,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+                
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        });
+        var sliderToggle = true;
+        $('.slider-start_stop').on('click', function() {
+          if(sliderToggle) {
+            $('.banner-slide').slick('slickPause'); 
+              $(this).text('자동재생').addClass('on');
+          } else {
+            $('.banner-slide').slick('slickPlay'); 
+              $(this).text('일시정지').removeClass('on');
+          }
+          sliderToggle = !sliderToggle;
+        });
+      }
+  
       
-      dots: false,
-      infinite: true,
-      arrows: true,
-      autoplay : true,
-      autoplaySpeed: 1000,
-      easing: 'linear',
-      cssEase: 'linear',
-      speed: 1000,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      prevArrow : ".banner-slick-prev",		
-      nextArrow : ".banner-slick-next",
-      responsive: [
-        {
-          breakpoint: 1006,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-    
-    
-    $('.slider-start_stop').on('click').toggle(function() {
-      $('.banner-slide').slick('slickPause'); 
-    }, function() {
-      $('.banner-slide').slick('slickPlay'); 
-    });
 
     $('#header .mobile_menu_list>ul>li>a').off().on('click', function() {
         $(this).parent().toggleClass('on').siblings('li').removeClass('on');
@@ -136,5 +148,32 @@
     $('#footer .family_site_wrap .family_site button').off().on('click', function() {
       $(this).parent().toggleClass('active');
       $('#footer .family_site_wrap .family_site_list').slideToggle();
+    });
+
+    // 팝업
+    popUp();
+
+    function popUp() {
+      $('.popup').hide();
+
+      $('#footer .foot_menu li:first-child a').on('click', function() {
+        $('.popup').show();
+      });
+     
+      
+
+      $('.popup .popup_inner .close_btn').on('click', function() {
+        $('.popup').hide();
+      });
+    }
+
+    // 서브페이지 
+    $('.sub_container .sub_lnb .section_inner .lnb_list li').mouseenter(function() {
+        $(this).parent().addClass('active');
+        $(this).children('.lnb_depth_2').show();
+    }).mouseleave(function() {
+      $(this).parent().removeClass('active');
+      $(this).children('.lnb_depth_2').hide();
     })
+   
 })();
